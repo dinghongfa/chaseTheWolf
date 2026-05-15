037bf59cdf782ab10c0894e229f5652a26e6a61c
dinghongfa <286540194@qq.com>
2026-05-15 22:44:52 +0800

    增加提示框。

--- application.4488e.js
+++ application.4488e.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.a9a3e.json';
+          this.settingsPath = 'src/settings.aa6df.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
---
+++ assets/resources/index.e5f9d.js
@@ -0,0 +1,20 @@
+System.register("chunks:///resources.js",["./rollupPluginModLoBabelHelpers-5fba63c8.js","cc"],(function(){var t,e,n,r,o,a,c,i,l;return{setters:[function(o){t=o._,e=o.a,n=o.b,r=o.c},function(t){o=t.cclegacy,a=t._decorator,c=t.Label,i=t.Sprite,l=t.Component}],execute:function(){var u,s,b,p,f,m,_;o._RF.push({},"c2616kUcb9Mbaks3EQ6zAX0","toastUI",void 0);var g=a.ccclass,y=a.property;u=g("toastUI"),s=y(c),b=y(i),u((m=t((f=function(t){function o(){for(var e,o=arguments.length,a=new Array(o),c=0;c<o;c++)a[c]=arguments[c];return e=t.call.apply(t,[this].concat(a))||this,n(e,"m_lab",m,r(e)),n(e,"m_bg",_,r(e)),e.m_txt="",e}e(o,t);var a=o.prototype;return a.onEnable=function(){this.m_txt=this.m_lab.string},a.update=function(t){},o}(l)).prototype,"m_lab",[s],{configurable:!0,enumerable:!0,writable:!0,initializer:function(){return null}}),_=t(f.prototype,"m_bg",[b],{configurable:!0,enumerable:!0,writable:!0,initializer:function(){return null}}),p=f));o._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/resources', 'chunks:///resources.js'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
